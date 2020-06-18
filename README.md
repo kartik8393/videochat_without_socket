4c7038280f69d90dd82f1dff5dcb6ec8eed3b2e7
kartik8393 <kartik.avertatech@gmail.com>
2020-06-18 12:22:48 +0530

    Seperated Init, not Init. Used express.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # videochat_without_socket
+
+1. npm install
+2. we are using watchify to give node like functionality on client side. It will create bundle.js file and we will use this file in index.html 
+3. run the command - npx watchify ./public/index.js -o ./public/bundle.js in terminal 1.
+4. run the command - npx watchify ./public/notInit.js -o ./public/bundle_notInit.js in terminal 2.
+5. We need command 3,4 if you are going to make changes in index.js and notInit.js only.
+6. run the command - npm start
+7. open localhost:3000/index.html in one tab, localhost:3000/notInit.html
+
---
+++ public/index.js
@@ -0,0 +1,42 @@
+navigator.getUserMedia({
+    video:true,
+    audio:false
+},function(stream){
+
+
+var Peer=require('simple-peer')
+var peer=new Peer({
+    initiator : true,
+    trickle:false,
+    stream: stream
+})
+
+peer.on('signal',function(data){
+    document.getElementById('yourId').value=JSON.stringify(data)
+})
+
+document.getElementById('connect').addEventListener('click',function(){
+    var otherId=JSON.parse(document.getElementById('otherId').value)
+    peer.signal(otherId)
+})
+
+document.getElementById('send').addEventListener('click',function(){
+    var yourMessage=document.getElementById('yourMessage').value
+    peer.send(yourMessage)
+})
+
+peer.on('data',function(data){
+    document.getElementById('messages').textContent += data +'\n'
+})
+
+peer.on('stream',function(stream){
+    var video=document.createElement('video')
+    //document.body.appendChild(video)
+    //video.src=window.URL.createObjectURL(stream)
+    video.srcObject=stream
+    document.querySelector('#peerDiv').appendChild(video)
+    video.play()
+})
+},function(err){
+    console.log(err)
+})
